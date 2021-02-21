55ca8e81ec08efabf1b7d5e842486422dbd12bd8
rohuna <agrawal.rohun@gmail.com>
2021-02-21 13:15:27 -0800

    f

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ db.collection("users").get().then(snap => {
     snap.forEach(doc => {
         html += `<div class = "col-sm-4">
             <img src = ${doc.data().imgurl} style = "width: 80%; margin: 10%;">
-            <h5>Name: ${doc.data().name } <br>Age: ${doc.data().age }</h5> </div>
+            <h5 style = "text-align: center;">Name: ${doc.data().name } <br>Age: ${doc.data().age }</h5> </div>
         `
     })
     document.getElementById("gallery").innerHTML = html;
